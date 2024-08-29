b3665ae06e42bffb848835d2c0cdad9f2cbb863c
Mejabin <mehjabin.elu11@gmail.com>
2024-08-29 16:34:04 +0600

    msg get

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express= require('express')
-const app =express();
-const http=require('http');
-const expressServer= http.createServer(app);
-app.get("/",function(req,res){
-    res.send(__dirname+"/index.html");
-})
+const express = require("express");
+const app = express();
+const http = require("http");
+const expressServer = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(expressServer);
 
-expressServer.listen(3000,function(){
-    console.log("server run @3000")
-})
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function (socket) {
+  
+  socket.on("chat message", function (msg) {
+    io.emit('chat_transfer',msg)
+    console.log(msg); 
+  });
+});
+
+expressServer.listen(3000, function () {
+  console.log("Server running on port 3000");
+});
